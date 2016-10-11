8f444b7bec6b87224a294dfb2d62dcea7ad61bce
= <samsmits@gmail.com>
2016-10-11 16:05:43 -0700

    Added Heatmap Visualization

---
+++ Visualizations/Heatmap/js/heatmap.js
@@ -0,0 +1,129 @@
+/**
+ *
+ * @source: http://github.com/samsmits/SonnenburgLab_Public/Visualizations/Heatmap/js/heatmap.js
+ *
+ * @licstart  The following is the entire license notice for the 
+ *  JavaScript code in this page.
+ *
+ * Copyright (C) 2015 Sam Smits
+ *
+ *
+ * The JavaScript code in this page is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GNU GPL) as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version.  The code is distributed WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+ *
+ * As additional permission under GNU GPL version 3 section 7, you
+ * may distribute non-source (e.g., minimized or compacted) forms of
+ * that code without the copy of the GNU GPL normally required by
+ * section 4, provided you include this license notice and a URL
+ * through which recipients can access the Corresponding Source.
+ *
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this page.
+ *
+ */
+
+Heatmap = function(data, optionalArgs){
+  this.data = data;
+
+  // Defaults
+  this.width = 300;
+  this.height = 300;
+  this.margin = {top: 0, right: 0, bottom: 0, left: 0};
+
+  this.boxWidth = 10;
+  this.boxHeight = 10;
+  this.boxPadding = 1;
+
+  this.showHeader = false;
+  this.headerBoxHeight = optionalArgs.hasOwnProperty('boxHeight') ? optionalArgs['boxHeight'] + 8 : this.boxHeight + 8
+  this.headerBoxWidth = optionalArgs.hasOwnProperty('boxWidth') ? optionalArgs['boxWidth'] : this.boxWidth
+
+  this.colorScale = d3.scale.linear()
+    .domain([0, 0.2, 0.5, 0.7, 1])
+    .range(['#FFF', '#eacac5', '#d093a3', '#a7658d', '#6b3e6f', '#2e1f3f']);
+
+  this.headerColorScale = d3.scale.category10();
+
+  /* Overwrite defaults with custom settings */
+  for(var key in optionalArgs){
+    if(optionalArgs.hasOwnProperty(key)){
+      this[key] = optionalArgs[key];
+    }
+  }
+
+  /* Post-Definition processing */
+  this.width -= this.margin.left - this.margin.right,
+  this.height -= this.margin.top - this.margin.bottom;  
+
+  this.x = d3.scale.linear()
+    .domain([0, this.cols])
+    .range([0, this.width]);
+
+  this.y = d3.scale.linear()
+    .domain([this.rows, 0])
+    .range([this.height, 0]);
+
+}
+
+Heatmap.prototype.renderTo = function(dom){
+  var that = this;
+
+  this.svg = d3.select(dom).append("svg")
+    .attr("width", this.width + this.margin.left + this.margin.right)
+    .attr("height", this.height + this.margin.top + this.margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
+
+  for( var i = 0; i < data.length; i++ ){
+    that._renderRow(i)
+  }    
+
+  if( that.showHeader ){
+    that._renderHeader(that.showHeader)
+  }
+}
+
+Heatmap.prototype._renderHeader = function(data){
+  var that = this;
+
+  this.heatmapRow = this.svg.selectAll('rect .header')
+    .data(data)
+    .enter()  
+    .append('rect')
+    .attr('width', that.headerBoxWidth)
+    .attr('height', that.headerBoxHeight)
+    .attr('fill', function(d,i){ 
+      switch(d){
+        case "Bacteroidetes":
+          return "#339933";
+        case "Firmicutes":
+          return "#ff9900";
+      }
+      return "#CCCCCC";
+    } )
+    .attr('x', function(d,i){ return (that.boxPadding + that.boxWidth) * i })
+    .attr('y', function(){  return 0; });
+
+}
+
+Heatmap.prototype._renderRow = function(n){
+  var that = this;
+
+  this.heatmapRow = this.svg.selectAll('rect .row' + n)
+    .data(that.data[n])
+    .enter()  
+    .append('rect')
+    .attr('width', that.boxWidth)
+    .attr('height', that.boxHeight)
+    .attr('fill', function(d){ return that.colorScale(d); })
+    .attr('fill-opacity', function(d){ return d == 0 ? 0 : 1 })
+    .attr('stroke', '#000000')
+    .attr('stroke-width', function(d){ return d == 0 ? '0' : '0.1' })    
+    .attr('x', function(d,i){ return (that.boxPadding + that.boxWidth) * i })
+    .attr('y', function(){  return (n + 2) * (that.boxPadding + that.boxHeight) });
+}
---
+++ Visualizations/Heatmap/readme.md
@@ -0,0 +1,78 @@
+# Heatmap vis using D3
+
+I generated a Heatmap [pseudo-]class in Javascript for the extinction paper. There were some visual elements that I included in the paper (background coloring of rows, headers, lines between groups) that were partly hackishly written and in some cases hard-coded in order to generate quickly. I've stripped most of those elements from the code, leaving the main Heatmap class from which we can build up.
+
+## Dependencies
+
+The viz depends on [D3](http://www.d3js.com).  
+
+The viz generates a SVG element. There is a [really neat bookmark](http://nytimes.github.io/svg-crowbar/) that you can add to your bookmarks bar, that on clicking gives you the option of saving the graphic as a SVG. This is handy if you want to import the resulting visualization into Adobe Illustrator, for instance.
+
+## Data Structure
+
+
+### Cell data
+
+You need to supply the Heatmap class with a vector of vectors (or list of lists) for the heatmap data. This makes the library able to consume data from JSON output easily, and additionally simple to handle outputs from python. 
+
+Example:
+```javascript
+	data = [
+		[0.15, 0.21, 0.90, 0.11, 0.45, 0.38, 0.12],
+		[0.34, 0.38, 0.98, 0.00, 0.00, 0.53, 0.43],
+		[0.12, 0.54, 0.32, 0.21, 0.00, 0.54, 0.55],
+		[0.99, 0.11, 0.33, 0.21, 0.12, 0.23, 0.43],
+		[0.10, 0.34, 0.11, 0.34, 0.99, 0.44, 0.34]
+	]
+```
+
+### Headers
+
+This was something of an afterthought when generating the viz for the publication. As such, this data object is passed to the class as a separate list. If you don't pass a headers list, none is rendered.
+
+Example:
+```javascript
+headers = ["Firmicutes","Firmicutes","Firmicutes","Bacteroidetes","Verrucomicrobia","Firmicutes","Firmicutes"];
+```
+
+## Sample code
+
+The following renders a heatmap using default visual elements to a <div> with id="graph":
+```javascript
+graph = new Heatmap(data);
+graph.renderTo('#graph');
+
+The following renders a heatmap with headers defined, and unique sized cells:
+```javascript
+graph = new Heatmap(
+	data,
+	{ 
+		width: 12 * data[0].length + 100, 	// These are the dimensions of the entir visualization. Make sure
+		height: 12 * data.length + 100,			// to give sufficient padding so that it doesn't get cropped.
+		boxWidth: 10,												// This is the width of each cell
+		boxHeight: 10,											// This is the height of each cell
+		showHeader: headers									// We pass the header list to render
+	}
+);
+graph.renderTo('#graph');	
+```
+
+You can set your own color scale to work with your dataset by passing a colorscale function:
+```javascript
+graph = new Heatmap(
+	data,
+	{ 
+		colorScale: d3.scale.linear()
+	    .domain([0, 1])
+	    .range(['#FFF', '#000'])
+	}
+);
+graph.renderTo('#graph');	
+```
+
+
+
+
+
+
+
